4080ef3196291ed708a940259af1dce30540a54d
Konter <kandereev270@gmail.com>
2025-11-05 13:30:00 +0300

    last update on notebook 05.11.2025

--- bot_aiogram/bot_aiogram.py
+++ bot_aiogram/bot_aiogram.py
@@ -1,4 +1,17 @@
-a = "Text"
-print(a)
-print()
-print("22323")
+import asyncio
+import time
+
+async def send_email(num):
+    print(f'Email sended {num}')
+    await asyncio.sleep(1)
+    print(f'Email {num} dostavleno')
+
+
+start_time = time.time()
+
+async def main():
+    task = [send_email(i) for i in range(10)]
+    await asyncio.gather(*task)
+    print(f'Time elasped: {time.time() - start_time} s')
+
+asyncio.run(main())
