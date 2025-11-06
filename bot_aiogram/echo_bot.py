c2dff8abfef4d162bf5af40789371ceb1f25d322
konter <kandreev270@gmail.com>
2025-11-06 22:48:12 +0300

    06/11/2025

---
+++ bot_aiogram/echo_bot.py
@@ -0,0 +1,24 @@
+﻿from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+with open(r"..\.venv\env") as file:
+	Bot_token = file.readline()
+
+bot = Bot(token=Bot_token)
+dp = Dispatcher()
+
+@dp.message(Command(commands="start"))
+async def proc_command_start (message: Message):
+	await message.answer("Привет! Это эхо бот")
+
+@dp.message(Command(commands="help"))
+async def proc_command_help (message: Message):
+	await message.answer("Этот бот возвращает то, что ты ему пишешь")
+
+@dp.message()
+async def echo_send (message: Message):
+	await message.reply(text=message.text)
+
+if __name__ == '__main__':
+	dp.run_polling(bot)
---
+++ bot_aiogram/update_bot.py
@@ -0,0 +1,26 @@
+﻿import requests
+import time
+
+API_URL = 'https://api.telegram.org/bot'
+with open(r"..\.venv\env") as file:
+	BOT_TOKEN = file.readline()
+TEXT = 'Ура! Классный апдейт!'
+
+def update_cat():
+    update = requests.get('https://api.thecatapi.com/v1/images/search').json()
+    for res in update:
+        return(res['url'])
+    
+foto_cat:str = update_cat()
+
+offset = -2
+chat_id: int
+
+updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+
+for result in updates['result']:
+    offset = result['update_id']
+    chat_id = result['message']['from']['id']
+    requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+    requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={foto_cat}')
